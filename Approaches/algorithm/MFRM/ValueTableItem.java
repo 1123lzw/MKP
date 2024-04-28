c3982f4004fae05ea08a4da0c1e1b74679f4184f
ZiWei-Luo <52397004+ZiWei-Luo@users.noreply.github.com>
2024-04-28 20:04:32 +0800

    Add files via upload

---
+++ Approaches/algorithm/MFRM/ValueTableItem.java
@@ -0,0 +1,46 @@
+package MFRM;
+
+import java.util.ArrayList;
+
+public class ValueTableItem {
+    private int tID = 0; // 事务编号
+    private ArrayList<Integer> optNums = new ArrayList<Integer>();// 物品编号集合
+    private double objVal = 0d;// 价值
+
+    public ValueTableItem(ArrayList<Integer> optNums, double objVal) {
+        this.optNums = optNums;
+        this.objVal = objVal;
+
+    }
+
+    public ValueTableItem(Integer[] optNums, double objVal) {
+        for (Integer optNum : optNums) {
+            this.optNums.add(optNum);
+        }
+        this.objVal = objVal;
+    }
+
+    public int getTID() {
+        return tID;
+    }
+
+    public void setTID(int tID) {
+        this.tID = tID;
+    }
+
+    public ArrayList<Integer> getOptNums() {
+        return optNums;
+    }
+
+    public void setOptNums(ArrayList<Integer> optNums) {
+        this.optNums = optNums;
+    }
+
+    public double getObjVal() {
+        return objVal;
+    }
+
+    public void setObjVal(double objVal) {
+        this.objVal = objVal;
+    }
+}
